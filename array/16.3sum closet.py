551a1e8ea78d0679c4a58b85d6ae3ac61c383311
denotepython <1134568611@qq.com>
2016-11-19 12:49:48 +0800

    类似于三个数求和
    每次找出差值，找到最小地差值对应地和，不用考虑去重的问题

---
+++ array/16.3sum closet.py
@@ -0,0 +1,28 @@
+class Solution(object):
+	def threeSumClosest(self, nums, target):
+		length = len(nums)
+		ans = []
+		nums = sorted(nums)
+		closet = 10000000
+		for i in xrange(length-2):
+			if i == 0 or nums[i] != nums[i-1]:
+				j, k = i + 1, length - 1
+				while j < k:
+					diff = nums[i] + nums[j] + nums[k] - target#这一句位置在while循环内
+					if abs(diff) < closet:
+						closet = abs(diff)
+						ans = nums[i] + nums[j] + nums[k]
+					if diff < 0 :
+						j += 1
+					elif diff > 0:
+						k -= 1
+					else:
+						print nums[i], nums[j], nums[k],
+						j += 1
+						k -= 1
+		return ans
+nums = [0, 2, 1, -3]
+target = 1
+a = Solution()
+ans = a.threeSumClosest(nums, target)
+print ans
