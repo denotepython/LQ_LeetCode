01a4b6e3f1b843936d2ef5125b84ec50fe3d0be8
denotepython <1134568611@qq.com>
2016-12-05 10:03:16 +0800

    证明了leetcode中可以导入自己想要用的包
    这样可以更加方便地用numpy等包，有的题目应该可以提升效率

--- array/59.spiral matrix II.py
+++ array/59.spiral matrix II.py
@@ -1,22 +1,52 @@
+# class Solution(object):
+# 	def generateMatrix(self, n):
+# 		cols, rows = n, n
+# 		matrix = [[0 for col in range(cols)] for row in range(rows)]
+# 		level = n
+# 		k, down, right, up, left = level, level, level, 0, 0
+# 		count = 1
+# 		while k > 1 and left <= right and up <= down:
+# 			for i in xrange(left,right):
+# 				matrix[up][i] = count
+# 				count += 1
+# 			for i in xrange(up+1,down-1):
+# 				matrix[i][right-1] = count
+# 				count += 1
+# 			for i in xrange(left,right):
+# 				matrix[down-1][level-i-1] = count
+# 				count += 1
+# 			for i in xrange(up+1,down-1):
+# 				matrix[level-i-1][left] = count
+# 				count += 1
+# 			up += 1
+# 			down -= 1
+# 			left += 1
+# 			right -= 1
+# 			k -= 2
+# 		if k == 1:
+# 			matrix[level/2][level/2] = count
+# 		return matrix
+
+import numpy as np  #可以导入想用的包
+#主要是证明了这个方法的可行性
 class Solution(object):
 	def generateMatrix(self, n):
-		cols, rows = n, n
-		matrix = [[0 for col in range(cols)] for row in range(rows)]
+		temp = np.zeros((n,n), dtype=np.int)
 		level = n
 		k, down, right, up, left = level, level, level, 0, 0
 		count = 1
 		while k > 1 and left <= right and up <= down:
 			for i in xrange(left,right):
-				matrix[up][i] = count
+				temp[up][i] = count
 				count += 1
 			for i in xrange(up+1,down-1):
-				matrix[i][right-1] = count
+				temp[i][right-1] = count
 				count += 1
 			for i in xrange(left,right):
-				matrix[down-1][level-i-1] = count
+				temp[down-1][level-i-1] = count
 				count += 1
 			for i in xrange(up+1,down-1):
-				matrix[level-i-1][left] = count
+				temp[level-i-1][left] = count
 				count += 1
 			up += 1
 			down -= 1
@@ -24,7 +54,8 @@ class Solution(object):
 			right -= 1
 			k -= 2
 		if k == 1:
-			matrix[level/2][level/2] = count
+			temp[level/2][level/2] = count
+		matrix = temp.tolist() #这样有时候一些二维数组的创建更加方便
 		return matrix
 
 if __name__ == "__main__":
