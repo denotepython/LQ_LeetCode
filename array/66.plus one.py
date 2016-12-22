3a9f43baff8ee1f2206d17a3057c90e307ef05dc
denotepython <1134568611@qq.com>
2016-12-22 12:41:33 +0800

    66.plus one
    数组转化为数进行加一计算
    另外VS code 可以GIT,虽然没有提交到github。
    最后配置了VScode运行python环境

--- array/66.plus one.py
+++ array/66.plus one.py
@@ -1,20 +1,20 @@
 class Solution(object):
     def plusOne(self, digits):
-        length = len(digits)
         num = 0
-        for i in xrange(length-1,-1):
-            num += num * 10 + digits[i]
+        for i in digits[::]:
+            num = num * 10 + i
         num += 1
         ans = []
         while num != 0:
-            ans.append(num/10)
+            ans.append(num % 10)
+            num /= 10
         ans.reverse()
-        return num
+        return ans
         """
         :type digits: List[int]
         :rtype: List[int]
         """
 
 if __name__=="__main__":
-    result = Solution().plusOne([1,4,5])
+    result = Solution().plusOne([1,4,5,9])
     print result
