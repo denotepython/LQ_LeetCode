0b25f3d4c7d60ce47212148c536b19f24a31ce09
denotepython <1134568611@qq.com>
2016-12-31 15:42:17 +0800

    rotate image
    尝试按行求逆再转置的方法

--- array/48.rotate image.py
+++ array/48.rotate image.py
@@ -1,3 +1,5 @@
+# -*- coding:utf-8 -*-
+
 class Solution(object):
 	def rotate(self, matrix):
 		length = len(matrix)
@@ -9,10 +11,26 @@ class Solution(object):
 		for i in xrange(length):
 			for j in xrange(length):
 				matrix[i][j] = temp[i][j]
+		return matrix
 		#也不能用matrix = temp，这样matrix映射到新的id，原来的id值没变
 
+'''
+import numpy as np
+class Solution(object):
+	def rotate(self, matrix):
+		matrix = np.matrix(matrix)
+		length = len(tmp)
+		i, j = 0, length-1
+		while i < j:
+			tmp[[i,j]] = tmp[[j,i]]
+			i += 1
+			j -= 1
+		return tmp.transpose()
+'''
+
 if __name__ == "__main__":
-	result = Solution().rotate([[1,2],[3,4]])
+	result = Solution().rotate([[1,2,3],[4,5,6], [7,8,9]])
 	print result
+
 
 
